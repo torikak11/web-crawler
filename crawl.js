e4bf8f9fe03c2b963d3582f2ec51fdc0237ccb35
torikak11 <victoriakratzke@gmail.com>
2023-04-03 15:21:00 +0100

    crawl over multiple pages

--- crawl.js
+++ crawl.js
@@ -19,7 +19,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
         for (const aElement of aElements) {
             if (aElement.href.slice(0, 1) === '/') {
                 try {
-                    const urlObj = new URL(baseURL + aElement.href.slice(1));
+                    const urlObj = new URL(aElement.href, baseURL);
                     urls.push(urlObj.href);
                 } catch (err) {
                     console.log(`error with relative url: ${err.message}`);
@@ -27,7 +27,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
             } else {
                 try {
                     const urlObj = new URL(aElement.href);
-                    urls.push(aElement.href);
+                    urls.push(urlObj.href);
                 } catch (err) {
                     console.log(`error with absolute url: ${err.message}`);
                 }
@@ -38,31 +38,42 @@ function getURLsFromHTML(htmlBody, baseURL) {
 }
 
 async function crawlPage(baseURL, currentURL, pages) {
-    try {
-        const response = await fetch(currentURL);
-        if (response.status > 399) {
-            console.log(`error with status code: ${response.status} on page: ${currentURL}`);
-            return
-        }
-        const contentType = response.headers.get("content-type");
-        if (!contentType.includes("text/html")) {
-            console.log("error in content-type");
-            return
-        }
-        console.log(await response.text())
-    } catch (err) {
-        console.log(err.message);
-        return
-    }
     const currentURLObj = new URL(currentURL);
     const baseURLObj = new URL(baseURL);
     if (currentURLObj.hostname !== baseURLObj.hostname) {
         return pages
     }
+
     const normalizedCurrentURL = normalizeURL(currentURL);
-    if (normalizedCurrentURL in pages) {
-        
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL] ++;
+        return pages
     }
+
+    pages[normalizedCurrentURL] = 1;
+    console.log(`crawling over: ${currentURL}`);
+
+    let htmlBody = '';
+    try {
+        const response = await fetch(currentURL);
+        if (response.status > 399) {
+            console.log(`error with status code: ${response.status} on page: ${currentURL}`);
+            return pages
+        }
+        const contentType = response.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            console.log("error in content-type");
+            return pages
+        }
+        htmlBody = await response.text();
+    } catch (err) {
+        console.log(err.message);
+    }
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages);
+    }
+    return pages
 }
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const { crawlPage } = require("./crawl.js");
 
-function main() {
+async function main() {
     // Check for valid amount of comman line arguments
     if (process.argv.length < 3 || process.argv.length > 3) {
         console.log("Invalid number of arguments: Program requires 1 base URL");
     } else {
         console.log(`Web crawler has started at ${process.argv[2]}`);
     }
-    crawlPage(process.argv[2], process.argv[2], {});
+    const pages = await crawlPage(process.argv[2], process.argv[2], {});
 
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 }
 
 main()
